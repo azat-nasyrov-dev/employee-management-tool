70d4f5e8d981eeacc574528186bd475ab361c4b3
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-02-08 04:23:30 +0600

    connect pipes globally

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -6,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
+  );
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
