16e3fb6031d5111922189b3bfc26c64e27109913
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-02-10 19:23:08 +0600

    add basic routes in client

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'employees',
+  },
+  {
+    path: 'employees',
+    loadChildren: () =>
+      import('./features/employees/employees.routes').then(
+        (m) => m.EMPLOYEES_ROUTES,
+    ),
+  },
+  {
+    path: 'tags',
+    loadChildren: () =>
+      import('./features/tags/tags.routes').then(
+        (m) => m.TAGS_ROUTES,
+      ),
+  },
+];
