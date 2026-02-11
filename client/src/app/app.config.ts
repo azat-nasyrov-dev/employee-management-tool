c795385419e7996f3d4224d6678a19dd75dd5580
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-02-11 23:48:37 +0600

    update app config in client

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { routes } from './app.routes';
 import { CORE_PROVIDERS } from './core/core.providers';
+import { httpErrorInterceptor } from './core/interceptors/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     ...CORE_PROVIDERS,
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([httpErrorInterceptor])
+    ),
+    importProvidersFrom(
+      MatSnackBarModule,
+    ),
   ]
 };
