b6de69972e155492e26d9e80658b8ba9ffd48d58
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-02-11 23:46:06 +0600

    add interceptor for handling error

---
+++ client/src/app/core/interceptors/http-error.interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, throwError } from 'rxjs';
+
+export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const snackBar = inject(MatSnackBar);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      const message =
+        error.error?.message ||
+        error.message ||
+        'Unexpected error occurred';
+
+      snackBar.open(message, 'Close', {
+        duration: 4000,
+      });
+
+      return throwError(() => error);
+    })
+  );
+};
