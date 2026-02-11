4b60db6cad8b3234ff1b48469154130a997e9ec1
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-02-11 23:15:27 +0600

    add lazy routes to tags feature

---
+++ client/src/app/tags/tags.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TagsPageComponent } from './pages/tags-page.component';
+
+export const TAGS_ROUTES: Routes = [
+  {
+    path: '',
+    component: TagsPageComponent,
+  },
+];
