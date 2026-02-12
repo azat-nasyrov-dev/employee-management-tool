1021ce48b55c55e77f8e43834688e9f2337cb488
Azat Nasyrov <azat.nasyrov31@gmail.com>
2026-02-13 05:10:44 +0600

    add lazy routes to employees feature

---
+++ client/src/app/employees/employees.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { EmployeesPageComponent } from './pages/employees-page.component';
+
+export const EMPLOYEES_ROUTES: Routes = [
+  {
+    path: '',
+    component: EmployeesPageComponent,
+  },
+];
